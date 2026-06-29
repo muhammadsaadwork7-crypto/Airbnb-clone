e800397f12586fd8e3a1b341c7cea09063eb2e8d
Saad <muhammadsaad.work7@gmail.com>
2026-06-29 18:18:18 +0500

    making login and signup routes

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
+});
+
+router.post("/signup",wrapAsync(async(req,res) => {
+    try {
+        let {username,email,password} = req.body;
+        const newUser = new User({email,username});
+        const registeredUser = await User.register(newUser,password);
+        req.flash("success","Welcome to WanderLust!");
+        res.redirect("./listings");
+    } catch (error) {
+        req.flash("error",error.message);
+        res.redirect("/signup");
+    }
+}));
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true
+}), async (req, res) => {
+    req.flash("success", "Welcome back! You are logged in!");
+    res.redirect("/listings");
 });
 
 module.exports = router;
