8241059546e2ca599a8f5d6e5ac4b63f8863ccf7
Saad <muhammadsaad.work7@gmail.com>
2026-07-01 23:49:27 +0500

    doing login logout functionality

--- routes/user.js
+++ routes/user.js
@@ -33,4 +33,14 @@ router.post("/login", passport.authenticate("local", {
     res.redirect("/listings");
 });
 
+router.get("/logout", (req, res) => {
+    req.logout((err) => {
+    if (err) {
+        return next(err);
+    }
+    req.flash("success", "you are logged out!"); 
+    res.redirect("/listings");
+    });
+});
+
 module.exports = router;
