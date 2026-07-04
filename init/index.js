8939e20a43b93834c182de50e2065ca50a7423b0
Saad <muhammadsaad.work7@gmail.com>
2026-07-05 00:29:35 +0500

    add owner to every listing

--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj,owner: '6a40fd94415abdcd88af0bc4'}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
