3442f951f701d0c06ccafcb2942fbd999ef69046
Saad <muhammadsaad.work7@gmail.com>
2026-07-05 00:32:05 +0500

    commit

--- routes/listing.js
+++ routes/listing.js
@@ -30,7 +30,7 @@ router.get("/new", isLoggedIn, (req,res)=>{
 //Show Route
 router.get("/:id",wrapAsync(async (req,res)=>{
     let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if(!listing){
         req.flash("error","Listing does not exist");
         res.redirect("/listings");
@@ -44,6 +44,7 @@ router.post("/",
         validateListing,
         wrapAsync(async (req,res)=>{
         const newListing = new Listing(req.body.listing);
+        newListing.owner = req.user._id;
         await newListing.save();
         req.flash("success","New Listing Created");
         res.redirect("/listings");
