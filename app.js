dc1e43925c341623020036103d64df3f3b12c7bb
Saad <muhammadsaad.work7@gmail.com>
2026-07-18 16:58:33 +0500

    shift to mongo db atlas

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 if(process.env.NODE_ENV != "production"){
     require('dotenv').config();
 }
+const dns = require("dns");
+dns.setServers(["1.1.1.1"]);   // Cloudflare DNS
 
 const express = require("express");
 const app = express();
@@ -10,6 +12,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo').default;
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -19,7 +22,9 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLABDB_URL;
+
 
 main().then(()=>{
     console.log("connected to Db");
@@ -28,7 +33,7 @@ main().then(()=>{
 })
 
 async function main() {
-    await mongoose.connect(MONGO_URL);  
+    await mongoose.connect(dbUrl);  
 };
 
 app.set("view engine","ejs");
@@ -38,7 +43,20 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: "mysupersecretcode"
+    },
+    touchAfter: 24*3600,
+});
+
+store.on("error",()=>{
+    console.log("ERROR in SESSION STORE",err);
+})
+
 const sessionOptions = {
+    store,
     secret : "mysupersecretcode",
     resave: false,
     saveUninitialized: true,
